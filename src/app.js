5a5bf8cfc33462744fd1445b9451687b83e175d1
Neville <nevillegilor@gmail.com>
2025-11-18 16:34:10 +0100

    ygg

--- src/app.js
+++ src/app.js
@@ -72,17 +72,6 @@ app.get('/create', (req, res)=>{
 
 });
 
-app.get('/deleteVideogame', (req, res)=>{
-    res.render('deleteVideogame');
-});
-
-app.get('/uploadVideogame', (req, res)=>{
-    res.render('uploadVideogame');
-});
-
-app.get('/modifyComment', (req, res)=>{
-    res.render('modifyComment');
-});
 
 
 
